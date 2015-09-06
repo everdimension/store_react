1fd1cc5a11178375ae5a12f54566878d1f337a05
everdimension <everdimension@gmail.com>
2015-09-06 03:59:40 +0300

    move bootstrap.less to the styles folder; markup <Item /> elements

---
+++ src/components/Items/Item.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Item extends React.Component {
+	render() {
+		const item = this.props.item;
+		return (
+			<div className="panel panel-default">
+				<div className="panel-body">
+					<div className="media">
+						<div className="media-left">
+							<img src={item.img} alt="item image" className="media-object" />
+							</div>
+
+							<div className="media-body">
+
+								<div className="row">
+
+									<div className="col-sm-4">
+										<p>{item.name}</p>
+										<p>{item.color}</p>
+									</div>
+									<div className="col-sm-4">
+										<p> Issue date: {item.issueDate}</p>
+										<p>
+											<span className="glyphicon glyphicon-ok text-success"></span>
+											In stock
+										</p>
+									</div>
+									<div className="col-sm-4">
+										<p>{item.price}</p>
+										<p>
+											<button className="btn btn-success" ng-disabled="!item.inStock" ng-click="CartSvc.addItem(item)" id={'cart_item-' + item._id}>
+												Order
+											</button>
+										</p>
+									</div>
+
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+		);
+	}
+}
+
+export default Item;
--- src/components/Items/ItemsList.js
+++ src/components/Items/ItemsList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Item from './Item.js';
 
 class ItemsList extends React.Component {
 
 	render() {
 		let loadingText = this.props.isLoading ? 'loading...' : '';
+		let items = this.props.items.map((item) => {
+			return <Item key={item._id} item={item} />;
+		});
 		return (
 			<div className="ItemsList">
 				{loadingText}
-				{this.props.items.map((item) => <div>{item.name}</div>)}
+				{items}
 			</div>
 		);
 	}
